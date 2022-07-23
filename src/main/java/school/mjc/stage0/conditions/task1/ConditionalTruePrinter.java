ee8dd91c9f29ef477214cbd5c6939d1c463b2ea5
Yoqub <yyboboraximov@gmail.com>
2022-07-23 14:08:51 +0500

    task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        if (a==b){
+            System.out.println(true);
+        }
     }
 }
